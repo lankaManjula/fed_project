01a039bbc01ce7bd7a04c97502fd1f1c61f9e6c7
lankaManjula <lanka.manjula@marlink.com>
2024-11-26 14:05:46 +0100

    Add download

---
+++ fed/download.py
@@ -0,0 +1,4 @@
+import opendatasets as od
+
+# Download dataset from Kaggle using username and the key.
+od.download("https://www.kaggle.com/datasets/ashishpatel26/facial-expression-recognitionferchallenge", data_dir='data')
